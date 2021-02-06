7716fc3cc3acb54eb4230ba21b02f994dca6ead5
Arnav-Chaudhary <71707268+Arnav-Chaudhary@users.noreply.github.com>
2021-02-07 00:18:12 +0530

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,52 @@
+book="";
+object=[];
+status="";
+function preload(){
+    book = loadImage('Book.jpg');
+}
+function setup(){
+canvas= createCanvas(380,380);
+canvas.center();
+
+objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="Status: Dectecting Objects";
+}
+ function modelLoaded(){
+console.log("Model Loaded");
+status= true;
+}
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{  
+    object=results; 
+    console.log(results);
+} 
+
+}
+
+function draw(){
+    
+    image(book,0,0,380,380);
+    if(status != "" ){
+        r= random(255);
+        g= random(255);
+        b= random(255);
+        objectDetector.detect(book,gotResult);
+        for(i=0; i < object.length;  i++ ){
+            document.getElementById("status").innerHTML="Status: Object Detected ";
+            document.getElementById("object_length").innerHTML= "Number Of Objects Detected Are : " + object.length;
+            percent= floor(object[i].confidence * 100);
+            fill(r,g,b);
+          text(object[i].label + " " + percent + "%" ,   object[i].x , object[i].y);
+         noFill();
+         stroke(r,g,b);
+         rect(object[i].x , object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
+function back(){
+    window.location="Index.html"
+}
---
+++ Main.js
@@ -0,0 +1,18 @@
+function firstimg(){
+    window.location="Laptop.html";
+}
+function scndimg(){
+    window.location="Mouse.html"
+}
+function thirdimg(){
+    window.location="Book.html"
+}
+function fourimg(){
+    window.location="CD.html"
+}
+function fiveimg(){
+window.location="Card.html"
+}
+function siximg(){
+    window.location="Clock.html"
+}
